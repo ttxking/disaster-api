0af356939756c019bd74fab3be18d3374a9905f2
Anusid W <anusid.w@ku.th>
2021-11-23 16:41:47 +0700

    commit on main.py

---
+++ app.py
@@ -0,0 +1,45 @@
+import sys
+import os
+
+if not os.path.exists("config.py"):
+    print("Configuration 'config.py' not found.  "
+          "You may create one from 'config.py.example'.")
+    exit(1)
+
+from config import OPENAPI_AUTOGEN_DIR
+
+if not os.path.exists(OPENAPI_AUTOGEN_DIR):
+    print("Folder '{}' not found.  "
+          "Execute openapi-generator to generate it, "
+          "e.g.,".format(OPENAPI_AUTOGEN_DIR))
+    print()
+    print("  "
+          "java -jar openapi-generator-cli-X.Y.Z.jar generate \\\n"
+          "     -i openapi/disaster-api.yaml \\\n"
+          "     -o {} -g python-flask".format(OPENAPI_AUTOGEN_DIR))
+    print()
+    exit(1)
+
+sys.path.append(OPENAPI_AUTOGEN_DIR)
+
+try:
+    import connexion
+except ModuleNotFoundError:
+    print("Please install all required packages by running:"
+          " pip install -r requirements.txt")
+    exit(1)
+
+from openapi_server import encoder
+
+def main():
+    app = connexion.App(__name__, specification_dir='./openapi/')
+    app.app.json_encoder = encoder.JSONEncoder
+    app.add_api('rain-api.yaml',
+                arguments={'title': 'Thailand Disaster API'},
+                pythonic_params=True)
+
+    app.run(port=8080, debug=True)
+
+
+if __name__ == '__main__':
+    main()
